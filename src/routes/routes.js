9f345e3e23934ed5314200edc71ef623568c68f9
Emanoel Loreto <emanoel.loreto@meta.com.br>
2022-05-17 18:03:01 -0300

    src/routes/routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Index from '../pages/index/index';
+// import Index from '../pages/index/index';
 import Details from '../pages/details/details';
 import Insert from '../pages/insert/insert';
 import Update from '../pages/update/update';
@@ -10,7 +10,7 @@ const Routes = () => (
     
     <BrowserRouter>
         <Switch>
-            <Route exact path = "/" component={Index} />
+            {/* <Route exact path = "/" component={Index} /> */}
             <Route path = "/usuarios/:id" component={Details} />
             <Route path = "/criarusuario" component={Insert} />
             <Route path = "/editarusuario/:id" component={Update} />
