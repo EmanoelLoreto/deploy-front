dc96a1b8b66a0e1c26521d4fb9460eefb9ace3ec
Emanoel Loreto <emanoel.loreto@meta.com.br>
2022-05-17 18:21:14 -0300

    vai caralho

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/header/header';
+import Routes from './routes/routes';
+import './App.css';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <Header />
+      <Routes />
+    </div>
   );
 }
 
