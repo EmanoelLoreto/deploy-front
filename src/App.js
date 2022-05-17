094b1bf00d7dc234352380c02e8d35071e0cb4a2
Emanoel Loreto <emanoel.loreto@meta.com.br>
2022-05-17 18:16:23 -0300

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-// import Header from './components/header/header';
-// import Routes from './routes/routes';
-// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <h1>Hello</h1>
   );
 }
 
