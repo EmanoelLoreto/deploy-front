3baaf499176ed8bf6ca2062fbe287d52cb3956a4
EmanoelLoreto <EmanoelLoreto@users.noreply.github.com>
2022-05-17 20:45:35 +0000

    deploy: 1ba7037c578207c2d7d76de2580606b4406bc93e

---
+++ precache-manifest.f89f44ca9bcb9e74370c3f1c48aef523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecd51792c35e53debd8b974ec5efa23",
+    "url": "deploy-front/index.html"
+  },
+  {
+    "revision": "656ec5a5b220068b5e6d",
+    "url": "deploy-front/static/css/main.7ab8adc6.chunk.css"
+  },
+  {
+    "revision": "36fe14f6ddca0af9eeb1",
+    "url": "deploy-front/static/js/2.4e8368c8.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "deploy-front/static/js/2.4e8368c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "656ec5a5b220068b5e6d",
+    "url": "deploy-front/static/js/main.8c27a205.chunk.js"
+  },
+  {
+    "revision": "af2f844b0c0ec27a8471",
+    "url": "deploy-front/static/js/runtime-main.f947e3e3.js"
+  }
+]);
